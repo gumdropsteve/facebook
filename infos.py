71d46fa04eae99f8f508952a028208c30b042cc2
Winston <winston@Winstons-MacBook-Pro.local>
2019-07-06 03:29:25 -0700

    through login

--- infos.py
+++ infos.py
@@ -1,1 +1,7 @@
-# to store paths and other infos utilized by facebook class 
+# fb login page
+fb_login = 'https://facebook.com/login/'
+
+# "email or phone number" input box; xpath
+login_box = '//*[@id="email"]'
+# password input box; xpath
+pass_box = '//*[@id="pass"]'
