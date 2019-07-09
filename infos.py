4fa53b298d5a72fa9da23238b558faf8890f05d3
Winston <winston@Winstons-MacBook-Pro.local>
2019-07-09 16:18:45 -0700

    added: link for ph4s fb page, xpath for post_box, xpath for post_button

--- infos.py
+++ infos.py
@@ -1,7 +1,13 @@
 # fb login page
 fb_login = 'https://facebook.com/login/'
+# pleasanton homes for sale page
+ph4s = 'https://www.facebook.com/pleasantonhome/'
 
 # "email or phone number" input box; xpath
 login_box = '//*[@id="email"]'
 # password input box; xpath
 pass_box = '//*[@id="pass"]'
+# page post box; xpath
+post_box = '//*[@aria-label="Write a post..."]'
+# 'share now' post button; xpath
+post_button = '//span[@id="composerPostButton"]'
