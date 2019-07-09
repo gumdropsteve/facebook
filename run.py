93ea546e2bd50fe41fa6fa5e2c5af63999a1125d
Winston <winston@Winstons-MacBook-Pro.local>
2019-07-09 16:22:17 -0700

    sample run of fbbotw/ new methods; user may add sleep throughout for visibility when testing/learning --- allows reusability much more than prior bot formatting

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 from user import u,p
 from base import fbBot
+from infos import ph4s
+
 if __name__=='__main__':
+    # tag facebook bot
     fb=fbBot(username=u,password=p)
+    # start up driver, keep window large
+    fb.start_driver(minimize=False)
+    # log in to fb
     fb.login()
+    # load ph4s page
+    fb.load_page(url=ph4s)
+    # set status
+    msg='Hello, World!'
+    # write status
+    fb.write_post(text=msg)
+    # post status
+    fb.post_now()
+    # close up shop
     fb.close_browser()
     
